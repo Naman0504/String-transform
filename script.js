46335c315e9c7fcff461dac45e58f7141fbfcdae
Naman0504 <matiyaranaman@gmail.com>
2025-08-02 23:46:46 +0530

    transfroming functionailty of string in js

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
 const input = document.querySelector(".input");
 const submitBtn = document.querySelector(".submitBtn");
-const transformedText = document.querySelector(".transformed-text");
+const Upper_Case = document.querySelector(".upper-case");
+const Lower_Case = document.querySelector(".lower-case");
+const Kebab_Case = document.querySelector(".kebab-case");
+const Pascal_Case = document.querySelector(".pascal-case");
+const Camel_Case = document.querySelector(".camel-case");
 
-
-submitBtn.addEventListener("click",(e)=>{
-
-  console.log(input.value)
+submitBtn.addEventListener("click", (e) => {
+  console.log(input.value);
   const value = input.value;
-  transformedText.innerText = value.toLowerCase();
+  upperCase(value);
+  lowerCase(value);
+  kebabCase(value);
+  pascalCase(value);
+  camelCase(value);
+  input.value = "";
+});
 
-  input.value =""
+function upperCase(str) {
+  const upperCase = str.toUpperCase();
+  Upper_Case.innerText = upperCase;
+}
 
-})
+function lowerCase(str) {
+  const lowerCase = str.toLowerCase();
+  Lower_Case.innerText = lowerCase;
+}
+function kebabCase(str) {
+  const kebabCase = str.toLowerCase().split(" ").join("-");
+  Kebab_Case.innerText = kebabCase;
+}
+function pascalCase(str) {
+  let pascalCase = str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join("");
+  Pascal_Case.innerText = pascalCase;
+}
+function camelCase(str) {
+  let camelCase = str
+    .toLowerCase()
+    .split(" ")
+    .map((word, index) =>
+      index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)
+    )
+    .join("");
+    Camel_Case.innerText = camelCase;
+}
